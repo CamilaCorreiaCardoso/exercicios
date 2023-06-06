e104639a2d6b647fe467e06159bf7bacc21563ad
CamilaCorreiaCardoso <camilacorreiacardoso@hotmail.com>
2023-06-06 20:04:25 -0300

    commit inicial

---
+++ condicionais/script.js
@@ -0,0 +1,43 @@
+numero = 5
+if (numero === 7) {
+    a = "seja bem vindo"
+    console.log(a)
+} else if (numero === 5) {
+    b = "retorne again"
+    console.log(b)
+}
+else {
+    c = "Nenhum dos dois"
+    console.log(c)
+}
+//Faça um programa que some dois numeros e exiba uma mensagem com 
+//o resultado apenas se o resultado for maior que 100. Se for menor que 100,
+//imprima a mensagem "menor que 100".
+a = 2
+b = 3
+soma = a + b
+if (soma > 100) {
+    d = "Maior que 100"
+    console.log(d, soma)
+}
+else if (soma < 100) {
+    e = "Menor que 100"
+    console.log(e)
+}
+//Faça um programa que multiplica dois números e imprime uma mensagem 
+//informando se o resultado deu "positivo" ou "negativo".
+a = 5
+b = -9
+resultado = a * b
+if (resultado > 0) {
+    d = "positivo"
+    console.log(d)
+}
+else if (resultado === 0) {
+    f = "Neutro"
+    console.log(f)
+}
+else {
+    e = "negativo"
+    console.log(e)
+}
---
+++ variaveis/script.js
@@ -0,0 +1,14 @@
+//Faça um programa que imprima na tela a mensagem "Olá mundo!"
+console.log("Hello world!")
+//Faça um programa que some dois números e i9cmprima na tela (utilize variáveis)
+a = 5
+b = 4
+c = a + b
+console.log(c)
+//Faça um programa que calcule a média de 3 números (use variáveis)
+a = 5
+b = 7
+c = 6
+console.log((a + b + c) / 3)
+
+
