8e84e74828426c75269fc0144096d170ea4a2f43
CamilaCorreiaCardoso <105814793+CamilaCorreiaCardoso@users.noreply.github.com>
2023-07-11 19:57:13 -0300

    Create script.js

---
+++ EstruturaDeDados/script.js
@@ -0,0 +1,13 @@
+//Uma arquiteta precisa criar um modelo 3D de sofá para utilizar na planta 3D que ela está desenhando graficamente. Para fazer esse modelo ela está utilizando o Autocad. Internamente, o Autocad internamente utiliza uma estrutura de código para representar esse modelo do sofá (material, cor, quantidade de braços...). Utilize uma estrutura de código para recriar a representação do sofá no código semelhante ao que seria no Autocad.
+//Sofá
+//material
+// cor
+// quantidade de braço
+
+const sofa= { 
+    material:"couro",
+    cor:"vinho"
+    
+  
+}
+ console.log("O material é de " + (sofa.material) +" e na cor "+(sofa.cor))
