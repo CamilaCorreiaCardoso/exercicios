653ff16c3b0c88e17a2a703a4f1ddd6f9275049f
CamilaCorreiaCardoso <105814793+CamilaCorreiaCardoso@users.noreply.github.com>
2023-07-25 23:28:58 -0300

    estruturas

--- EstruturaDeDados/script.js
+++ EstruturaDeDados/script.js
@@ -20,3 +20,44 @@ console.log(sofa)
       quantidadeDeBraco: 4,
       disponivelParaVenda: false 
  }
+
+//Uma locadora precisa de um sistema para gerenciar todas as locações e carros que estão disponíveis ou locados. Construa uma estrutura para representar a entidade Carro dessa locadora. A partir desta estrutura, construa três exemplos diferentes de carros. Dentre os atributos do Carro, é indispensável ter a marca do carro, quilômetros rodados, ano do carro, valor de locação, se o carro está locado ou não, placa do carro. Imprima os atributos de cada carro em cada linha.
+
+const carro01 = {
+ marca: "hilux",
+ km: "18000",
+ ano: "2016",
+ valor: 450.00,
+ estaDisponivel: true,
+placa:"JRB01496"
+}
+
+const carro02 = {
+marca: "hondaCivic",
+ km: "38000",
+ ano: "2018",
+ valor: 480.00,
+ estaDisponivel:false,
+placa:"JQC4506"
+}
+
+const carro03 = { 
+    marca: "corola",
+    km: "12000",
+    ano: "2023",
+    valor: 680.00,
+    estaDisponivel:false,
+    placa:"QTV1624"
+}
+console.log(carro01)
+console.log(carro02)
+console.log(carro03)
+
+
+if (!carro02.estaDisponivel){
+    console.log("carro locado")
+}
+
+if (carro01.estaDisponivel){
+    console.log("carro disponível")
+}
